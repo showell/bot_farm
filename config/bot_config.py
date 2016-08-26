80c174e34cf1dbe71f6620dfba1d4fa74e14de74
Steve Howell <showell30@yahoo.com>
2016-08-26 08:01:02 -0700

    Add bot_config.py

---
+++ config/bot_config.py
@@ -0,0 +1,13 @@
+BOTS = [
+    dict(
+        name='followup',
+        lib='zulip/contrib_bots/lib/followup.py',
+        zuliprc='config/zuliprc-prod',
+    ),
+    dict(
+        name='help',
+        lib='zulip/contrib_bots/lib/help.py',
+        zuliprc='config/zuliprc-prod',
+    ),
+]
+
--- deployments/supervised/startup.py
+++ deployments/supervised/startup.py
@@ -1,5 +1,9 @@
+import os
 import subprocess
 import sys
+
+def get_root_path():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
 SUPERVISORD_HELP = '''
 SUCCESS!
@@ -23,11 +27,42 @@ You can shut down everything, including supervisord itself, as follows:
     supervisorctl shutdown
 '''
 
+WORKER_TEMPLATE = '''
+[program:bot-farm-%(name)s]
+command=python %(run_py)s %(lib_path)s --config-file %(zuliprc_path)s
+'''
+
+def join(dir, fn):
+    return os.path.abspath(os.path.join(dir, fn))
+
+def get_worker_config(bots, root_path):
+    '''
+    Return the supervisord configuration for our bot workers.
+    '''
+    text = '; CODE-GENERATED CONFIG FOLLOWS\n'
+    for bot in bots:
+        run_py = join(root_path, 'zulip/contrib_bots/run.py')
+        lib_path = join(root_path, bot['lib'])
+        zuliprc_path = join(root_path, bot['zuliprc'])
+        text += WORKER_TEMPLATE % dict(
+            run_py=run_py,
+            name=bot['name'],
+            lib_path=lib_path,
+            zuliprc_path=zuliprc_path)
+
+    return  text
+
 def run():
+    root_path = get_root_path()
+
+    sys.path.append('../../config')
+    from bot_config import BOTS
+    worker_config = get_worker_config(bots=BOTS, root_path=root_path)
+
     template_fn = 'supervisord.conf.template'
     template_payload = open(template_fn).read()
     conf_fn = 'supervisord.conf'
-    conf_payload = template_payload
+    conf_payload = template_payload % dict(worker_config=worker_config)
     open(conf_fn, 'w').write(conf_payload)
     try:
         result = subprocess.call([
